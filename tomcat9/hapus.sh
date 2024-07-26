750dc69180842d9179b1823fab3247919f0ea897
mtaabich <mtaabich@gmail.com>
2024-07-26 22:22:20 +0200

    deployment in place deployment conf

--- tomcat9/hapus.sh
+++ tomcat9/hapus.sh
@@ -1,5 +1,6 @@
 oc delete all -l app=tomcat-server
 oc delete template tomcat-server
-oc delete resourcequotas ompute-resources
+oc delete resourcequotas compute-resources
+oc delete limitranges core-resource-limits
 oc create -f tomcat9.yaml
 oc new-app tomcat-server --env-file=env-tomcat.properties
