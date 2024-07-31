9f2944f0121152cbecabd764c08e35d365e03446
mtaabich <mtaabich@gmail.com>
2024-07-31 11:38:26 +0200

    httpd

--- httpd/deploy.sh
+++ httpd/deploy.sh
@@ -1,7 +1,5 @@
-oc delete all -l httpd-server
+oc delete all -l app=httpd-server
 oc delete template httpd-template
-oc delete resourcequotas compute-resources
-oc delete limitranges core-resource-limits
 oc delete  persistentvolumeclaims pv-httpd-data
 oc create -f httpd.yaml
-oc new-app httpd-template --env-file=env-httpd.properties
+oc new-app app=httpd-template --env-file=env-httpd.properties
