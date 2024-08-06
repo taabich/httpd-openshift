3396b4ecb2d4fc748cfee62e5911cadaf5e0e227
mtaabich <mtaabich@gmail.com>
2024-08-06 21:16:13 +0200

    httpd

--- httpd/deploy.sh
+++ httpd/deploy.sh
@@ -4,3 +4,8 @@ oc delete  persistentvolumeclaims pv-httpd-data
 oc delete  persistentvolume pv-httpd
 oc create -f httpd.yaml
 oc new-app httpd-template --env-file=env-httpd.properties
+oc create sa sa-httpd
+oc adm policy add-scc-to-user anyuid -z sa-httpd
+oc adm policy add-scc-to-user anyuid -z sa-httpd -n oc-project
+oc set sa deployment httpd-server sa-httpd
+oc scale deploy httpd-server --replicas=1
