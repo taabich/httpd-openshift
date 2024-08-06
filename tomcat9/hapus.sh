16390d293a47b4676345a339501df34087d5f6a3
mtaabich <mtaabich@gmail.com>
2024-08-06 21:19:19 +0200

    httpd

--- tomcat9/hapus.sh
+++
@@ -1,9 +0,0 @@
-oc delete all -l app=tomcat-server
-oc delete template tomcat-server
-oc delete resourcequotas compute-resources
-oc delete limitranges core-resource-limits
-oc delete  persistentvolumeclaims pv-tomcat-data
-oc delete -f local-pv.yml
-oc create -f local-pv.yml
-oc create -f tomcat9.yaml
-oc new-app tomcat-server --env-file=env-tomcat.properties
