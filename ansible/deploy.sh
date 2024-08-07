81d85660a70bc10f7d6f579d95aa0bb42a141bab
mtaabich <mtaabich@gmail.com>
2024-08-07 10:23:38 +0200

    ansible

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -2,6 +2,7 @@ oc delete all -l app=ansible-server
 oc delete template ansible-template
 oc delete  persistentvolumeclaims pv-ansible-data
 oc delete  persistentvolume pv-ansibke
+oc delete sc  sa-ansible
 oc create -f ansible.yaml
 oc new-app ansible-template --env-file=env-ansible.properties
 oc create sa sa-ansible
